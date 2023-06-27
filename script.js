136eb8cbc9208d4d8aeabbd6ef5b4a467939a23c
Tejal Salve <60121669+tejalsalve19@users.noreply.github.com>
2023-06-27 16:45:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+let audio = null;
+
+function playSound(soundFile) {
+  if (audio !== null) {
+    audio.pause();
+    audio.currentTime = 0;
+  }
+
+  audio = new Audio("sounds/" + soundFile);
+  audio.play();
+}
+
+function stopSound() {
+  if (audio !== null) {
+    audio.pause();
+    audio.currentTime = 0;
+  }
+}
